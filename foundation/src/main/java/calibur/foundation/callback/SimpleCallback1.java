d3be1d0640d05ae9a2c5242d46f4da329f49f5f9
zhouruotong <zhouruotong@jianshu.com>
2018-11-18 10:17:49 +0800

    [feat]add some callbacks

---
+++ foundation/src/main/java/calibur/foundation/callback/SimpleCallback1.java
@@ -0,0 +1,12 @@
+package calibur.foundation.callback;
+
+/**
+ * author : J.Chou
+ * e-mail : who_know_me@163.com
+ * time   : 2018/11/18 9:34 AM
+ * version: 1.0
+ * description:
+ */
+public interface SimpleCallback1<T> {
+    void onCallback(T t);
+}
